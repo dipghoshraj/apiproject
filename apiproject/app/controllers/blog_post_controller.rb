e396620812d336f5655937142d000f6d89f0a191
dipghoshraj <dipghoshraj@gmail.com>
2020-10-21 14:05:53 +0530

    added routes

--- apiproject/app/controllers/blog_post_controller.rb
+++ apiproject/app/controllers/blog_post_controller.rb
@@ -1,2 +1,26 @@
 class BlogPostController < ApplicationController
+    # GET /post
+    def index
+        @post = BlogPost.all
+        render json: @post
+    end
+
+    def show
+        render json: @post
+    end
+
+    def create
+        @post = BlogPost.new(post_params)
+        if @post.save
+            render json: {message: "Post created successfully", post: @post}, status:200
+        else
+            render error: {error: "error to save the post"}, status:400
+        end
+    end
+
+    private
+    def post_params
+        params.require(:title).permit(:statement)
+    end
+
 end

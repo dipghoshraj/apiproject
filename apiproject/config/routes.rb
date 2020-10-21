423c13f2cf6345e520837531434f55b778b78c22
dipghoshraj <dipghoshraj@gmail.com>
2020-10-21 15:10:04 +0000

    added devise

--- apiproject/config/routes.rb
+++ apiproject/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :models
+  devise_for :users
+  devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/posts", to: "blog_post#index"
   post "/posts", to: "blog_post#create"

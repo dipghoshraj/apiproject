3ac73a84f6ab7d2dce1fed4fb829626f5a8ee84d
dipghoshraj <dipghoshraj@gmail.com>
2020-10-21 21:20:38 +0530

    Added delete api

--- apiproject/app/controllers/blog_post_controller.rb
+++ apiproject/app/controllers/blog_post_controller.rb
@@ -28,6 +28,17 @@ class BlogPostController < ApplicationController
         end
     end
 
+    def delete
+        @post = BlogPost.find(params[:id])
+        if @post
+            @post.destroy
+            render json: {message: "Post deleted successfully", post: @post}, status:200
+        else
+            render error: {error: "error to delete the post"}, status:400
+        end
+    end
+
+
     private
     def post_params
         params.require(:post).permit(:title, :statement)

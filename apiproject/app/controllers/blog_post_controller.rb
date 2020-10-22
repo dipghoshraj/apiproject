b03cc99b5104def9ac82edb19c01e6a0f6e9ff7a
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 22:36:59 +0530

    chnage validation

--- apiproject/app/controllers/blog_post_controller.rb
+++ apiproject/app/controllers/blog_post_controller.rb
@@ -19,7 +19,7 @@ class BlogPostController < ApplicationController
         # if params[:title] != nil
         @post = BlogPost.new(post_params)
         if @post.errors.blank? && @post.save
-            render json: {message: "Post created successfully", id: @post.id, post: @post}, status:201
+            render json: {message: "Post created successfully", id: @post.id}, status:201
         else
             render json: {message: "title or statement should not null or less than 3 characters"}, status:400
         end
@@ -34,7 +34,7 @@ class BlogPostController < ApplicationController
             @post.update_attributes(post_params)
             render json: {message: "Post updated successfully", post: @post.id}, status:200
         else
-            render error: {error: "error to update the post"}, status:400
+            render json: {message: "error to update the post"}, status:400
         end
     end
 

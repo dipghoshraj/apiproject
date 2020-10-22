e6d760d22128e4f581510013c23659e527ca8583
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 22:24:57 +0530

    change

--- apiproject/app/controllers/blog_post_controller.rb
+++ apiproject/app/controllers/blog_post_controller.rb
@@ -19,7 +19,7 @@ class BlogPostController < ApplicationController
         # if params[:title] != nil
         @post = BlogPost.new(post_params)
         if @post.errors.blank? && @post.save
-            render json: {message: "Post created successfully", id: @post.id}, status:201
+            render json: {message: "Post created successfully", id: @post.id, post: @post}, status:201
         else
             render json: {message: "title or statement should not null or less than 3 characters"}, status:400
         end

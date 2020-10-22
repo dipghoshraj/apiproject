86ef47c47c2e5edeff615f28a410da067207bea8
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 21:45:56 +0530

    chnage validation

--- apiproject/app/models/blog_post.rb
+++ apiproject/app/models/blog_post.rb
@@ -1,4 +1,4 @@
 class BlogPost < ApplicationRecord
-    validates :title, :blank_but_not_nil, presence: true, length: { minimum: 2 }, :allow_nil => false
-    validates :statement, :blank_but_not_nil, presence: true, length: { minimum: 2 }, :allow_nil => false
+    validates :title, presence: true, length: { minimum: 2 }, :allow_nil => false
+    validates :statement, presence: true, length: { minimum: 2 }, :allow_nil => false
 end

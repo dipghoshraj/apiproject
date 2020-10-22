8ea5de47235dcaad0c906a72a5dc41bed34392cf
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 18:04:20 +0530

    change the  upadated at

--- apiproject/app/models/application_record.rb
+++ apiproject/app/models/application_record.rb
@@ -4,4 +4,8 @@ class ApplicationRecord < ActiveRecord::Base
   def created_at
     attributes['created_at'].strftime("%m/%d/%Y %H:%M")
   end
+  
+  def updated_at
+    attributes['updated_at'].strftime("%m/%d/%Y %H:%M")
+  end
 end

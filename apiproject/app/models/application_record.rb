61984cf3c53bc0d7c17e42b6d904156428561c5c
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 18:11:47 +0530

    updated at chnage

--- apiproject/app/models/application_record.rb
+++ apiproject/app/models/application_record.rb
@@ -3,9 +3,6 @@ class ApplicationRecord < ActiveRecord::Base
 
   def created_at
     attributes['created_at'].strftime("%m/%d/%Y %H:%M")
-  end
-  
-  def updated_at
     attributes['updated_at'].strftime("%m/%d/%Y %H:%M")
   end
 end

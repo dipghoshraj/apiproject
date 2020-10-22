3a628babae509b54c8315d1b5ab4f20012c54649
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 22:24:49 +0530

    chmahe the validation

--- apiproject/app/controllers/blog_post_controller.rb
+++ apiproject/app/controllers/blog_post_controller.rb
@@ -18,7 +18,7 @@ class BlogPostController < ApplicationController
     def create
         # if params[:title] != nil
         @post = BlogPost.new(post_params)
-        if @post.errors.blank?
+        if @post.errors.blank? && @post.save
             render json: {message: "Post created successfully", id: @post.id}, status:201
         else
             render json: {message: "title or statement should not null or less than 3 characters"}, status:400

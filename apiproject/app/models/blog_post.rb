71ead0103df61c24245d9ff30aeb05fc4959660d
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 21:09:15 +0530

    Chnage the validation

--- apiproject/app/models/blog_post.rb
+++ apiproject/app/models/blog_post.rb
@@ -1,3 +1,5 @@
 class BlogPost < ApplicationRecord
     validates :title, :statement, :presence => true
+    validates :title, length: {minimum :3}
+    validates :statement, length: {minimum :3}
 end

79f8cd50e0f8748f7bb67dc67f364fd8ae22e74a
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 21:44:07 +0530

    chnage validation

--- apiproject/app/models/blog_post.rb
+++ apiproject/app/models/blog_post.rb
@@ -1,10 +1,4 @@
 class BlogPost < ApplicationRecord
-    validates :title, :blank_but_not_nil, presence: true, length: { minimum: 2 }
-    validates :statement, :blank_but_not_nil, presence: true, length: { minimum: 2 }
-
-    def blank_but_not_nil
-        if self.foo.nil?
-            errors.add :foo, 'cannot be nil'
-        end
-    end
+    validates :title, :blank_but_not_nil, presence: true, length: { minimum: 2 }, :allow_nil => false
+    validates :statement, :blank_but_not_nil, presence: true, length: { minimum: 2 }, :allow_nil => false
 end

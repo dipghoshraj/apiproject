3d9bfab9b094f789afec654f10febc65927a91a2
dipghoshraj <dipghoshraj@gmail.com>
2020-10-22 15:00:24 +0530

    change the route

--- apiproject/config/routes.rb
+++ apiproject/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/posts", to: "blog_post#index"
+  get "/posts/:id", to: "blog_post#show"
   post "/posts", to: "blog_post#create"
   post "/posts/update", to: "blog_post#update"
   delete "/posts", to: "blog_post#delete"

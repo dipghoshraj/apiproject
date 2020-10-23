83afc3da8ac07c3247407702ad65f5487b9b2b91
dipghoshraj <dipghoshraj@gmail.com>
2020-10-23 18:45:14 +0530

    fixing validation

--- apiproject/app/models/user.rb
+++ apiproject/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, length: { minimum: 6 }, :allow_nil => false, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :email, presence: true, length: { minimum: 6 }, :allow_nil => false, uniqueness: { case_sensitive: false }
+    validates :password, presence: true, length: { minimum: 6 }, :allow_nil => false
 end

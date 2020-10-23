7de272eafdb6da5232996753a1292ae92eaae80c
dipghoshraj <dipghoshraj@gmail.com>
2020-10-23 14:18:30 +0530

    fixing

--- apiproject/app/controllers/sessions_controller.rb
+++ apiproject/app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
         if @user
-            session[:user_id] = user.id
+            session[:user_id] = @user.id
             render json: {
                 status: :created,
                 logged_in: true,

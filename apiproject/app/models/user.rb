2dfd31fd2fa893ad27785456201013de8475c157
dipghoshraj <dipghoshraj@gmail.com>
2020-10-23 18:37:18 +0530

    validation chnage

--- apiproject/app/models/user.rb
+++ apiproject/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true, length: { minimum: 6 }, email: true, uniqueness: true, allow_nil => false
+    validates :email, presence: true, length: { minimum: 6 }, :allow_nil => false, email: true
 end

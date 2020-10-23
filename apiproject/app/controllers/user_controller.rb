bd99fee92c78ca8a1fa230be21f57f84f62df095
dipghoshraj <dipghoshraj@gmail.com>
2020-10-23 13:48:28 +0530

    added user

--- apiproject/app/controllers/user_controller.rb
+++ apiproject/app/controllers/user_controller.rb
@@ -1,2 +1,16 @@
 class UserController < ApplicationController
+
+    def create
+        @user = User.new(user_params)
+        if @post.errors.blank? && @post.save
+            render json: {message: "user created successfully", id: @user.id}, status:201
+        else
+            render json: {message: "something wwent wrong"}, status:403
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
--- apiproject/app/models/user.rb
+++ apiproject/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, length: { minimum: 6 }, :allow_nil => false
 end

459a13ed8cd764564d78913f2c5733e61eae6a16
dipghoshraj <dipghoshraj@gmail.com>
2020-10-23 08:01:35 +0000

    migrate:

--- apiproject/db/schema.rb
+++ apiproject/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_150844) do
+ActiveRecord::Schema.define(version: 2020_10_23_074646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,12 @@ ActiveRecord::Schema.define(version: 2020_10_21_150844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end

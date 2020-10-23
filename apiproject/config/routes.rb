c12612f0af0be95f10b026fb21e1f834fb52a902
dipghoshraj <dipghoshraj@gmail.com>
2020-10-23 13:15:10 +0530

    remove devise

--- apiproject/config/routes.rb
+++ apiproject/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :models
-  devise_for :users
-  devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/posts", to: "blog_post#index"
   get "/posts/:id", to: "blog_post#show"

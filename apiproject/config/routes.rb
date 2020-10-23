2f09603f3a183d3724a926c88310379694fdbabd
dipghoshraj <dipghoshraj@gmail.com>
2020-10-23 14:13:16 +0530

    added session

--- apiproject/config/routes.rb
+++ apiproject/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :sessions, only: [:create]
+  
   get "/posts", to: "blog_post#index"
   get "/posts/:id", to: "blog_post#show"
   post "/posts", to: "blog_post#create"

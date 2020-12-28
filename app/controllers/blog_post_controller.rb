71bbcbe9ec60177ea5d6f8581fb961139f71b53b
dipghoshraj <dip@spotkwik.com>
2020-12-29 01:23:12 +0530

    added image processor

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -22,7 +22,7 @@ class BlogPostController < ApplicationController
         # if params[:title] != nil
         @post = BlogPost.create(:title => params[:title], :statement => params[:statement])
         if @post.errors.blank? && @post.save
-            render json: {message: "Post created successfully", id: @post.id}, status:201
+            render json: {message: "Post created successfully", id: @post.id, post: @post}, status:201
         else
             render json: {message: "title or statement should not null or less than 3 characters"}, status:400
         end
